591215a79bb137d886641c8adbd3427f0dcb161b
git-marcopitra <marcomorete73@hotmail.com>
2020-09-28 10:34:46 +0100

    feat: scrollable fullpage

---
+++ examples/html/script.js
@@ -0,0 +1,48 @@
+let timeout, scrollIndex, scrollable, scrollBehavior;
+
+scrollable = true;
+scrollBehavior = false;
+
+const fp = document.querySelector(customFullpage || 'body');
+const html = document.querySelector('html');
+
+const resetScroll = () => scrollIndex = ~~(window.scrollY/window.innerHeight);
+const checkScrollBehavior = () => {
+    const finalPosition = fp.offsetHeight + fp.offsetTop;
+    const verticalPosition = window.scrollY;
+    const initialPosition =  fp.offsetTop;
+    const onElement = verticalPosition >= initialPosition && verticalPosition < finalPosition;
+
+    if (!scrollBehavior && onElement) {
+        html.style.scrollBehavior = 'smooth';
+        scrollBehavior = true;
+    } else if (scrollBehavior && !onElement) {
+        html.style.scrollBehavior = 'inherit';
+        scrollBehavior = false;
+    }
+}
+
+window.onload = () => {
+    resetScroll();
+    checkScrollBehavior();
+}
+
+window.onwheel = async (event) => {
+    checkScrollBehavior();
+    if (scrollable && scrollBehavior) {
+        if (event.deltaY > 0) scrollIndex++;
+        else scrollIndex--;
+        this.scrollTo(0, window.innerHeight * scrollIndex);
+        scrollable = false;
+    }
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+        scrollable = true;
+        resetScroll();
+        this.scrollTo(0, window.innerHeight * scrollIndex);
+    }, 50);
+}
+
+window.onscroll = () => {
+    checkScrollBehavior();
+}
